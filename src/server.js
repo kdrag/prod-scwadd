cb3770b6135439618a0686c4f216e17650776b85
John Lin <jlin@wirelessglue.com>
2022-05-11 15:10:06 -0700

    error stack logging added

--- src/server.js
+++ src/server.js
@@ -412,7 +412,7 @@ try {
                   return [...acc, ...intervalReading];
                 } else {
                   throw 'invalid item';
-                  console.log('Error - invalid content - xml file index, value: ', index, value);
+                  console.log('Error - invalid content - xml file index, value: ', index, value, err);
                 }
                return acc;
               },
@@ -457,7 +457,7 @@ try {
             }
          });
         } catch (error) {
-          console.log('On Error of item - xml file index, value: ', index, value);
+          console.log('On Error of item - xml file index, value: ', index, value, err);
         }
        // convert XML to JSON
       });
@@ -495,5 +495,5 @@ try {
   });
 
 } catch (error) {
-  console.log(error);
+  console.log(err);
 }
